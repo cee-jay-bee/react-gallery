a3b4045d97ca4479a7e26d56fa1063738fa5f59d
CJ Barnes <cristopher.barnes@gmail.com>
2021-10-30 16:40:02 -0500

    post Route completed with form to add

---
+++ src/components/PostImage/PostImage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {useState} from "react";
+import axios from "axios";
+
+function PostImage ( props ) {
+    // const [name, setName] = useState( null );
+    const [path, setPath] =  useState( '' );
+    const [description, setDescription] =  useState( '' );
+    
+    const handlePathChange = ( event ) => {
+        console.log('in handleURLChange', event.target.value );
+        setPath(event.target.value);
+    }
+
+    const handleDescChange = ( event ) => {
+        console.log('in handleDescChange', event.target.value );
+        setDescription(event.target.value);
+    }
+
+    const addImage = () => {
+        let imageToSend = {
+            path,
+            description
+        }
+        console.log('in addImage', imageToSend)
+        
+        axios.post(`/gallery`, imageToSend).then (( response )=>{
+            props.getImages();
+          }).catch( ( err )=>{
+            console.log( err );
+            alert( 'problem!' );
+          })
+    }
+
+    
+    
+
+    return(
+        <div>
+            <h1>Add Image to Gallery</h1>
+            <input type="text" placeholder="image url" onChange={ ( event )=> handlePathChange(event) } />
+            <input type="text" placeholder="description" onChange={ ( event )=> handleDescChange(event) } />
+            <button onClick={ addImage }>Add</button>
+        </div>
+    )
+}
+
+export default PostImage;
